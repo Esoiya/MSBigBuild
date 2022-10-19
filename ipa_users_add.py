5c43ec6607721cf335d3f55dc9837b31bdc15d53
Lyle-irvine <lyleirvine72@gmail.com>
2022-10-19 13:10:54 +0100

    Create ipa_users_add.py

---
+++ ipa_users_add.py
@@ -0,0 +1,9 @@
+from python_freeipa import ClientMeta
+import json
+import requests
+
+req = requests.get('http://94.237.60.14:8889/employees')
+print(req)
+# client = ClientMeta('ipaserver3.ee-bb.test')
+# client.login('admin', 'pinkEleph@nt!')
+# user = client.user_add()
