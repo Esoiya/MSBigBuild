589846874121d61984ddc1a988ec88b05fed5ade
Lyle-irvine <lyleirvine72@gmail.com>
2022-10-19 13:49:49 +0100

    Update ipa_users_add.py

--- ipa_users_add.py
+++ ipa_users_add.py
@@ -4,7 +4,7 @@ import requests
 
 #req = requests.get('http://94.237.60.14:8889/employees').json()
 
-client = ClientMeta('https://ipaserver3.ee-bb.test')
+client = ClientMeta('http://ipaserver3.ee-bb.test')
 client.login('admin', 'pinkEleph@nt!')
 
 created_users = client.user_find()
