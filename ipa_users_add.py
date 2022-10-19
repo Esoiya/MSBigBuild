583661653100fafc2b36217df10a8a110ffa9f0a
Lyle-irvine <lyleirvine72@gmail.com>
2022-10-19 13:24:32 +0100

    Update ipa_users_add.py

--- ipa_users_add.py
+++ ipa_users_add.py
@@ -2,9 +2,11 @@ from python_freeipa import ClientMeta
 import json
 import requests
 
-req = requests.get('http://94.237.60.14:8889/employees')
-print(req.json())
+req = requests.get('http://94.237.60.14:8889/employees').json()
 
-# client = ClientMeta('ipaserver3.ee-bb.test')
-# client.login('admin', 'pinkEleph@nt!')
+client = ClientMeta('ipaserver3.ee-bb.test')
+client.login('admin', 'pinkEleph@nt!')
+
+created_users = client.user_find()
+
 # user = client.user_add()
