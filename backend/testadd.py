fb15a81f9c493062933efb3a6bb59a6f7073d8ca
CK2411 <116077033+CK2411@users.noreply.github.com>
2022-10-19 11:57:42 +0100

    Update testadd.py

--- backend/testadd.py
+++ backend/testadd.py
@@ -1,31 +1,25 @@
-import db
+from db import DB
 import csv
 
 csv_file = "employees.csv"
 
 def insert_into_database():
-   csv_rows = []
-   with open(csv_file, newline='', encoding='utf-8-sig') as csvfile:
-      reader = csv.DictReader(csvfile)
-      for row in reader:
-         csv_rows.append(row)
-   line_number = 0
-   all_lines = []
-   line = ''
-   for row in csv_rows:
-      line += f'''( '{row['name']}', '{row['login']}', '{row['onboarded']}', '{row['dept']}'), '''
-      line_number += 1
-      if line_number == 500:
-         all_lines.append(line[:-2] + ';')
-         line_number = 0
-         line = ''
 
-   print(all_lines)
-   # with db.DB() as cursor:
-      # for line in all_lines:
-      #    sql = f'''INSERT INTO EMPLOYEE(name, login_id, onboarded, dept_id) VALUES {line}'''
-      #    print(sql)
-      #    cursor.execute(sql)
-      # cursor.commit()
+   with DB() as db:
+
+      with open(csv_file, 'r') as f:
+
+         reader = csv.DictReader(f)
+         for row in reader:
+            print(f"Inserting row: {row}")
+            query = f"""
+            INSERT INTO EMPLOYEE (login_id, dept_id, name, onboarded)
+            VALUES
+            ('{row['login']}', '{row['dept']}', '{row['name']}, '{row['onboarded']}')
+            """
+            print("Query: ", query)
+            db.execute(query)
+
+         db.commit()
 
 insert_into_database()
