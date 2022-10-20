267738de6161ad0e1cf91a0b520f7886b82a3203
Lyle-irvine <lyleirvine72@gmail.com>
2022-10-20 14:51:56 +0100

    Update ipa_single_user.py

--- backend/ipa_single_user.py
+++ backend/ipa_single_user.py
@@ -7,12 +7,15 @@ from quota import *
 
 from python_freeipa import ClientMeta
 
-def ipa_add_user(login_id, name, dept_id, onboarded):
-  urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+def ipa_connect():
   client = ClientMeta('ipaserver3.ee-bb.test', verify_ssl=False)
   client.login('admin', 'pinkEleph@nt!')
+  return client
 
+def ipa_add_user(login_id, name, dept_id, onboarded):
+  client = ipa_connect()
   fname, lname = (name.rstrip()).split(' ')
   full_name = name.rstrip()
   home_dir = os.path.join('/mnt/homes/', login_id)
@@ -23,3 +26,9 @@ def ipa_add_user(login_id, name, dept_id, onboarded):
   o_userpassword="TempPass")
   subprocess.run(["chown", login_id, home_dir])
   set_quota_for_user(login_id, 80, 100)
+
+
+def ipa_del_user(login_id):
+  client = ipa_connect()
+  client.user_disable(login_id)
+
